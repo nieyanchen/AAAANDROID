ad9765857a0c3fde5e095c95690b2f08cdfffab1
your name[core] <your email>
2019-05-22 21:41:33 +0800

    week12   sql

--- app/src/main/java/com/example/administrator/first/RateActivity.java
+++ app/src/main/java/com/example/administrator/first/RateActivity.java
@@ -1,14 +1,13 @@
 package com.example.administrator.first;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,238 +34,234 @@ public class RateActivity extends AppCompatActivity implements Runnable {
     private float dollarRate = 0.1f;
     private float euroRate = 0.2f;
     private float wonRate = 0.3f;
-    private String updateDate ="";
-
+    private String UpdateDate = "";
     EditText rmb;
     TextView show;
     Handler handler;
-    private MenuItem item;
-    private Element[] newsHeadlines;
 
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
-        rmb = (EditText) findViewById(R.id.rmb);
-        show = (TextView) findViewById(R.id.showOut);
+        rmb = findViewById(R.id.rmb);
+        show = findViewById(R.id.showOut);
 
-        /* 获取SP里保存的数据 */
-        SharedPreferences SharedPreferences = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        dollarRate = SharedPreferences.getFloat("dollar_rate",0.0f);
-        euroRate = SharedPreferences.getFloat("euro_rate",0.0f);
-        wonRate = SharedPreferences.getFloat("won_rate",0.0f);
-        updateDate = SharedPreferences.getString("update_date:","");
+        //获取SP里保存的数据
+        SharedPreferences a = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+        // SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        dollarRate = a.getFloat("dollar_rate",0.0f);
+        euroRate = a.getFloat("euro_rate",0.0f);
+        wonRate = a.getFloat("won_rate",0.0f);
+        UpdateDate = a.getString("update_date", "");
 
-        //获取当前系统时间
-        Date today =Calendar.getInstance().getTime();
-        SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd");
+        //获得当前系统时间
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         final String todayStr = sdf.format(today);
 
-        Log.i(TAG,"onCreate:sp dollarRate=" + dollarRate);
-        Log.i(TAG,"onCreate:sp euroRate=" + euroRate);
-        Log.i(TAG,"onCreate:sp wonRate=" + wonRate);
-        Log.i(TAG,"onCreate:sp updateDate=" + updateDate);
-        Log.i(TAG,"onCreate:todayStr=" + todayStr);
-
         //判断时间
-        if (todayStr.equals(updateDate)){
+        if(!todayStr.equals(UpdateDate)){
+
             //开启子线程
-            Log.i(TAG, "onCreate: 需要更新");
             Thread t = new Thread(this);
             t.start();
-        }else{
-            Log.i(TAG, "onCreate: 不需要更新");
         }
 
 
 
-        handler = new Handler() {
+
+        handler = new Handler(){
             @Override
-            public void handleMessage(Message msg){
-                if (msg.what==5){
+            public void handleMessage(Message msg) {
+                if(msg.what==5){
                     Bundle bdl = (Bundle) msg.obj;
                     dollarRate = bdl.getFloat("dollar-rate");
                     euroRate = bdl.getFloat("euro-rate");
                     wonRate = bdl.getFloat("won-rate");
-                    Log.i(TAG,"handleMessage:dollarRate:"+dollarRate);
-                    Log.i(TAG,"handleMessage:euroRate:"+euroRate);
-                    Log.i(TAG,"handleMessage:wonRate:"+wonRate);
 
                     //保存更新的日期
-                    SharedPreferences SharedPreferences = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = SharedPreferences.edit();
+                    SharedPreferences a = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = a.edit();
+                    editor.putFloat("dollar_rate",dollarRate);
+                    editor.putFloat("euro_rate",euroRate);
+                    editor.putFloat("won_rate",wonRate);
                     editor.putString("update_date",todayStr);
                     editor.apply();
-
-                    Toast.makeText(RateActivity.this,"汇率已更新",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RateActivity.this, "汇率已更新", Toast.LENGTH_SHORT).show();
                 }
                 super.handleMessage(msg);
             }
         };
     }
 
+
     public void onClick(View btn) {
-        //获取用户输入内容
-        Log.i(TAG,"onClick:");
         String str = rmb.getText().toString();
-        Log.i(TAG,"onClick:get str=" + str);
         float r = 0;
         if (str.length() > 0) {
             r = Float.parseFloat(str);
-        } else {
-            //提示用户输入内容
-            Toast.makeText(this, "请输入金额", Toast.LENGTH_SHORT).show();
+        }else{
+            //用户没有输入内容
+            Toast.makeText(this,"请输入金额",Toast.LENGTH_SHORT).show();
+            return;
         }
-        Log.i(TAG,"onClick: r=" + r);
+
+
+        //计算
 
         if (btn.getId() == R.id.btn_dollar) {
             show.setText(String.format("%.2f",r*dollarRate));
         } else if (btn.getId() == R.id.btn_euro) {
             show.setText(String.format("%.2f",r*euroRate));
-        } else if (btn.getId() == R.id.btn_won) {
+        } else {
             show.setText(String.format("%.2f",r*wonRate));
         }
 
     }
 
-    public void openOne(View btn) {
-        //打开一个页面Activity
-        //Log.i("open","openOne:");
-        //Intent hello = new Intent(this,SecondActivity.class);
-        //Intent web =new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jd.com"));//打开网页
-        //Intent intent =new Intent(Intent.ACTION_DIAL, Uri.parse("tel:17381561037"));//转到拨号
-        //startActivity(web);
+
+    public void openOne(View btn){
         openConfig();
     }
 
-    private void openConfig() {
-        Intent config = new Intent(this, ConfgActivity.class);
+    private void openConfig(){
+        Intent config = new Intent (this,ConfgActivity.class);
         config.putExtra("dollar_rate_key",dollarRate);
-        config.putExtra("dollar_rate_key",euroRate);
-        config.putExtra("dollar_rate_key",wonRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
 
-        Log.i(TAG,"openOne:dollarRate=" + dollarRate);
-        Log.i(TAG,"openOne:euroRate=" + euroRate);
-        Log.i(TAG,"openOne:wonRate=" + wonRate);
         startActivityForResult(config,1);
     }
 
     @Override
-    public  boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.rete, menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rete,menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.menu_set){
 
-        if(item.getItemId()==R.id.menu_set) {
             openConfig();
-    }else if(item.getItemId()==R.id.open_list){
+        }else if(item.getItemId()==R.id.open_list){
+
             //打开列表窗口
-            Intent list = new Intent(this, MyList2Activity.class);
+            Intent list = new Intent (this, RateListActivity.class);
             startActivity(list);
+
+//            //测试数据库
+//            RateItem item1 = new RateItem("aaa","123");
+//            RateManager manager = new RateManager(this);
+//            manager.add(item1);
+//            manager.add(new RateItem("bbb","23.5"));
+//
+//            //查询所有数据
+//            List<RateItem> testList = manager.listAll();
+//            for(RateItem i : testList){
+//
+//            }
+
+
+
         }
 
         return super.onOptionsItemSelected(item);
-
     }
 
-    protected void onActivityResult(int requestCode,int resultCode, Intent data){
-        if (requestCode==1&&resultCode==2){
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data ){
+        if(requestCode==1 && resultCode==2){
             Bundle bundle = data.getExtras();
             dollarRate = bundle.getFloat("key_dollar",0.1f);
             euroRate = bundle.getFloat("key_euro",0.1f);
             wonRate = bundle.getFloat("key_won",0.1f);
-            Log.i(TAG,"onActivityResult:dollarRate=" + dollarRate);
-            Log.i(TAG,"onActivityResult:euroRate=" + euroRate);
-            Log.i(TAG,"onActivityResult:wonRate=" + wonRate);
 
-            //将新设置的汇率写到SP中
-            SharedPreferences SharedPreferences = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
-            SharedPreferences.Editor editor = SharedPreferences.edit();
+            //将新设置的汇率写到SP里
+            SharedPreferences a = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+            SharedPreferences.Editor editor = a.edit();
             editor.putFloat("dollar_rate",dollarRate);
             editor.putFloat("euro_rate",euroRate);
             editor.putFloat("won_rate",wonRate);
             editor.commit();
-            Log.i(TAG,"onActiityResult:数据已保存到SharedPreferences");
-
-
-
         }
+
         super.onActivityResult(requestCode,resultCode,data);
     }
+
     @Override
     public void run() {
-        Log.i(TAG,"run:run()......");
-        for(int i = 1;i<3;i++) {
-            Log.i(TAG, "run:i=" + i);
-            try {
+        for(int i =1;i<6;i++){
+            try{
                 Thread.sleep(2000);
-            } catch (InterruptedException e) {
+            }catch(InterruptedException e){
                 e.printStackTrace();
             }
         }
 
-        //用于保存获取的汇率
+        //用户保存获取的汇率
         Bundle bundle;
 
 
+
         //获取网络数据
+
         /*URL url = null;
         try {
-            url = new URL("http://www.usd-cny.com/bankofchaina.htm");
+            url = new URL("http://www.usd-cny.com/bankofchina.htm");
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             InputStream in = http.getInputStream();
-            String html = inputSteam2String(in);
-            Log.i(TAG,"run:html ="+ html);
+
+            String html = inputStream2String(in);
             Document doc = Jsoup.parse(html);
-        }catch(MalformedURLException e){
+
+        } catch (MalformedURLException e) {
             e.printStackTrace();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }*/
 
         bundle = getFromBOC();
-        //获取msg对象 用于返回主线程
+
+
+
+        //bundle 中保存所获取的汇率
+
+        //获取Msg对象，用于返回主线程
         Message msg = handler.obtainMessage(5);
-       // msg.obj = "Hello from run()";
+        //msg.obj = "Hello from run()";
+        //b.sendMessage(msg);
         msg.obj = bundle;
         handler.sendMessage(msg);
+
     }
 
-    /**
-     * 从bankofchina获取数据
-     * @return
-     */
 
     private Bundle getFromBOC() {
         Bundle bundle = new Bundle();
         Document doc = null;
         try {
-            doc = Jsoup.connect("http://www.boc.com/sourcedb.whpj/").get();
-
-            Log.i(TAG,"run" + doc.title());
+            doc = Jsoup.connect("http://www.usd-cny.com/bankofchina.htm").get();
+            // doc = Jsoup.parse(html);
             Elements tables = doc.getElementsByTag("table");
-           /* for (Element table : tables){
-                Log.i(TAG,"run:table["+i+"]=" + table);
-                i++;
-            }*/
-            Element table2 = tables.get(1);
+
+            Element table1 = tables.get(0);
 
             //获取TD中的数据
-            Elements tds = table2.getElementsByTag("td");
-            for (int i =0;i<tds.size();i+=8){
+            Elements tds = table1.getElementsByTag("td");
+            for(int i = 0;i<tds.size();i+=6) {
                 Element td1 = tds.get(i);
-                Element td2 = tds.get(i+5);
-                Log.i(TAG,"run:text="+td1.text() + "==>" + td2.text());
+                Element td2 = tds.get(i + 5);
                 String str1 = td1.text();
                 String val = td2.text();
-                if("美元".equals(str1)) {
+
+                if ("美元".equals(str1)){
                     bundle.putFloat("dollar-rate", 100f / Float.parseFloat(val));
-                }else  if ("欧元".equals(str1)) {
+                } else if ("欧元".equals(str1)) {
                     bundle.putFloat("euro-rate", 100f / Float.parseFloat(val));
-                }else  if("韩国元".equals(str1)){
+                } else if ("韩元".equals(str1)) {
                     bundle.putFloat("won-rate", 100f / Float.parseFloat(val));
                 }
             }
@@ -276,55 +271,21 @@ public class RateActivity extends AppCompatActivity implements Runnable {
         return bundle;
     }
 
-    private Bundle getFromUsdCny() {
-        Bundle bundle = new Bundle();
-        Document doc = null;
-        try {
-            doc = Jsoup.connect("http://www.usd-cny.com/bankofchaina.htm").get();
 
-            Log.i(TAG,"run" + doc.title());
-            Elements tables = doc.getElementsByTag("table");
 
-           /* for (Element table : tables){
-                Log.i(TAG,"run:table["+i+"]=" + table);
-                i++;
-            }*/
-            Element table2 = tables.get(1);
 
-            //获取TD中的数据
-            Elements tds = table2.getElementsByTag("td");
-            for (int i =0;i<tds.size();i+=8){
-                Element td1 = tds.get(i);
-                Element td2 = tds.get(i+5);
-                Log.i(TAG,"run:text="+td1.text() + "==>" + td2.text());
-                String str1 = td1.text();
-                String val = td2.text();
-                if("美元".equals(str1)) {
-                    bundle.putFloat("dollar-rate", 100f / Float.parseFloat(val));
-                }else  if ("欧元".equals(str1)) {
-                    bundle.putFloat("euro-rate", 100f / Float.parseFloat(val));
-                }else  if("韩国元".equals(str1)){
-                    bundle.putFloat("won-rate", 100f / Float.parseFloat(val));
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return bundle;
-    }
-
-    private  String inputSteam2String(InputStream inputStream) throws IOException {
-        final  int bufferSize = 1024;
-        final char[] buffer = new  char[bufferSize];
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
         final StringBuilder out = new StringBuilder();
         Reader in = new InputStreamReader(inputStream,"gb2312");
-        for(;;){
+        for(; ;){
             int rsz = in.read(buffer,0,buffer.length);
-            if (rsz<0)
+            if(rsz<0)
                 break;
             out.append(buffer,0,rsz);
         }
-        return  out.toString();
+        return out.toString();
     }
 }
 
--- app/src/main/java/com/example/administrator/first/RateListActivity.java
+++ app/src/main/java/com/example/administrator/first/RateListActivity.java
@@ -1,6 +1,9 @@
 package com.example.administrator.first;
 
+import android.annotation.SuppressLint;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -14,34 +17,43 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static android.support.constraint.Constraints.TAG;
 
 public class RateListActivity extends ListActivity implements Runnable {
-    String data[] = {"waiting....."};
+    String data[] = {"wait..."};
     Handler handler;
+    private String logDate = "";
+    private final String DATE_SP_KEY = "lastRateDateStr";
+
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_rate_list);
-        final List<String>list1 = new ArrayList<String>();
-        for (int i = 1 ;i<100;i++){
-            list1.add("item"+i);
-        }
+        // setContentView(R.layout.activity_rate_list);
+        SharedPreferences sp = getSharedPreferences("myrate",Context.MODE_PRIVATE);
+        logDate = sp.getString(DATE_SP_KEY,"");
+        Log.i("list","lastRateDateStr=" + logDate);
 
-        ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        List<String> list1 = new ArrayList<String>();
+        for(int i=1;i<100;i++){
+            list1.add("item" + i);
+        }
+        ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list1);
         setListAdapter(adapter);
 
-        Thread t = new Thread();
+        Thread t = new Thread(this);
         t.start();
 
-        handler = new Handler(){
-            public void handleMessage(Message msg){
-                if (msg.what==7){
-                    List<String>list2 = (List<String>) msg.obj;
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if(msg.what==7){
+                    List<String> list2 = (List<String>) msg.obj;
                     ListAdapter adapter = new ArrayAdapter<String>(RateListActivity.this,android.R.layout.simple_list_item_1,list2);
                     setListAdapter(adapter);
                 }
@@ -49,43 +61,72 @@ public class RateListActivity extends ListActivity implements Runnable {
             }
         };
 
-
     }
 
     @Override
     public void run() {
-        //获取网络数据 当如list带回到主线程中
-        List<String> retlist = new ArrayList<String>();
-        Document doc = null;
-        try {
-            Thread.sleep(3000);
-            doc = Jsoup.connect("http://www.boc.com/sourcedb.whpj/").get();
+        //获取网络数据、放入list带回到主线程中
+        List<String> retList = new ArrayList<>();
+        String curDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
+        Log.i("run","curDateStr:" + curDateStr + "logDate:" + logDate);
 
-            Log.i(TAG,"run" + doc.title());
-            Elements tables = doc.getElementsByTag("table");
-        
-            Element table2 = tables.get(1);
-
-            //获取TD中的数据
-            Elements tds = table2.getElementsByTag("td");
-            for (int i =0;i<tds.size();i+=8){
-                Element td1 = tds.get(i);
-                Element td2 = tds.get(i+5);
-
-                String str1 = td1.text();
-                String val = td2.text();
-                Log.i(TAG,"run:"+str1 + "==>" + val );
-
+        if(curDateStr.equals(logDate)){
+            //如果相等，则不从网络中获取数据
+            Log.i("run","日期相等，从数据库中获取数据");
+            RateManager manager = new RateManager(this);
+            for(RateItem item : manager.listAll()){
+                retList.add(item.getCurName() + "-->" + item.getCurRate());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        }else{
+            //从网络获取数据
+            Log.i("run","日期不相等，从网络中获取数据");
+            Document doc = null;
+            try {
+                Thread.sleep(1000);
+                doc = Jsoup.connect("http://www.usd-cny.com/bankofchina.htm").get();
+                // doc = Jsoup.parse(html);
+                Elements tables = doc.getElementsByTag("table");
+
+                Element table1 = tables.get(0);
+
+                //获取TD中的数据
+                Elements tds = table1.getElementsByTag("td");
+                List<RateItem> rateList = new ArrayList<RateItem>();
+
+                for(int i = 0;i<tds.size();i+=6) {
+                    Element td1 = tds.get(i);
+                    Element td2 = tds.get(i + 5);
+
+                    String str1 = td1.text();
+                    String val = td2.text();
+
+                    Log.i(TAG,"run:" + str1 + "==>" + val);
+                    retList.add(str1 + "==>" + val);
+                    rateList.add(new RateItem(str1,val));
+                }
+
+                //把数据写入到数据库中
+                RateManager manager = new RateManager(this);
+                manager.deleteAll();
+                manager.addAll(rateList);
+
+                //记录更新日期
+                SharedPreferences sp = getSharedPreferences("myrate",Context.MODE_PRIVATE);
+                SharedPreferences.Editor edit = sp.edit();
+                edit.putString(DATE_SP_KEY, curDateStr);
+                edit.commit();
+                Log.i("run","更新日期结束：" + curDateStr);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
 
         Message msg = handler.obtainMessage(7);
-        msg.obj = retlist;
+        msg.obj = retList;
         handler.sendMessage(msg);
     }
 }
